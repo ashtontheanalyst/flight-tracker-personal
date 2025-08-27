8ec08debba2bccad00893d5b941eda356e4b9c3f
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-08-27 10:38:59 -0500

    Application now has user input that plugs into API call, returned object is now parsed for details

---
+++ README.md
@@ -0,0 +1,24 @@
+# Personal ADS-B Flight Tracking Application
+The plan for this application as of 27AUG25 is to create a web application that utilizes the OpenSky
+API (with account authorization) to display flight details in the greater CSTAT area.
+
+## OpenSky REST API Notes:
+Based on the [REST API Documentation](https://openskynetwork.github.io/opensky-api/rest.html#all-state-vectors), 
+this API call returns an object which is actually a 2D array. The collection of arrays contains arrays that have 
+aircraft data and telemetry, see the example below:
+
+```sh
+# This is the /all states query, boxed in with Long max/min and Lang max/min
+https://opensky-network.org/api/states/all?lamin=30.0707&lomin=-97.2864&lamax=31.3664&lomax=-95.4353
+```
+  
+```sh
+{"time":1756305312,"states":
+[["a728e2","N560WD  ","United States",1756305310,1756305311,-96.0715,30.6779,2872.74,false,165.3,162.24,-10.08,null,3055.62,null,false,0],
+... (more JSON) ...
+["a7f70e","N612KA  ","United States",1756305306,1756305306,-96.5878,30.8221,739.14,false,66.59,339.18,0.33,null,792.48,null,false,0]]}
+```
+
+## General Notes:
+- **Latitude:** is the horizontal imaginary lines on the globe, the equator is 0, below is neg. and above is pos.
+- **Longitude:** is the vertical lines, the prime meridian is 0, west (left) is neg. and east (right) is pos.
--- openSky.js
+++ openSky.js
@@ -1,54 +1,50 @@
-// Logic for the OpenSky API call for flight data
-// DOC: https://openskynetwork.github.io/opensky-api/rest.html
+// Personal Flight Tracker Using OpenSky API
 
-// CONSTANTS
-let USER;
-let PASS;
-const BASE = "https://opensky-network.org/api";
-const ALLSTATES = "/states/all";
+// This is our site logic, this all kicks off when we click the 'See Flights' button
+function callAPI() {
+  // CONSTANTS
+  let USER;
+  let PASS;
 
+  const LAMIN = document.getElementById("lamin").value;
+  const LOMIN = document.getElementById("lomin").value;
+  const LAMAX = document.getElementById("lamax").value;
+  const LOMAX = document.getElementById("lomax").value;
 
-// Fetch credentials. This function looks at that local file, gets the body in JSON, and assigns the
-// values from the JSON to our variables. We have an error statement in case of one.
-// NOTE: The clientSecret token resets every 30 minutes so you have to regenerate it from here: https://opensky-network.org/my-opensky/account
-fetch('credentials.json')
-  .then(response => response.json())
-  .then(data => {
-    USER = data.clientId;
-    PASS = data.clientSecret;
-  })
-  .catch(error => console.error("Error fetching Credentials:", error));
+  const BASE = "https://opensky-network.org/api";
+  const ALLSTATES = "/states/all";
+  const FULLURL = BASE + ALLSTATES + `?lamin=${LAMIN}&lomin=${LOMIN}&lamax=${LAMAX}&lomax=${LOMAX}`;
 
 
-// Latitude is the horizontal imaginary lines on the globe, the equator is 0, below is neg. and above is pos.
-// Longitude is the vertical lines, the prime meridian is 0, west (left) is neg. and east (right) is pos.
-let lamin;                  // lower lat bound
-let lomin;                  // lower long bound
-let lamax;                  // upper lat bound
-let lomax;                  // upper long bound
-
-// Boxing around greater CSTAT area, OpenSky likes 4 decimal points where possible
-lamin = 30.0707;
-lomin = -97.2864;           // Bastrop, TX
-lamax = 31.3664;
-lomax = -95.4353;           // Crockett, TX
-
-// Build the call
-const fullUrl = BASE + ALLSTATES + `?lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`;
+  // Fetch credentials. This function looks at that local file, gets the body in JSON, and assigns the
+  // values from the JSON to our variables. We have an error statement in case of one.
+  fetch('credentials.json') 
+    .then(response => response.json())
+    .then(data => {
+      USER = data.clientId;
+      PASS = data.clientSecret;
+    })
+    .catch(error => console.error("Error fetching Credentials:", error));
 
 
-// Function for the OpenSky API, it returns an array of the flights with a ton of data in browser console
-const openCall = async () => {
-  const response = await fetch(fullUrl, {
-    headers: {
-      "Authorization": "Basic" + btoa(`${USER}:${PASS}`)
-    }
-  });
-  
-  const data = await response.json(); //extract JSON from the http response
-  console.log(data);
+  // Function for the OpenSky API, it returns an array of the flights with a ton of data in browser console
+  const openCall = async () => {
+    const response = await fetch(FULLURL, {
+      headers: {
+        "Authorization": "Basic" + btoa(`${USER}:${PASS}`)
+      }
+    });
+    
+    const data = await response.json(); //extract JSON from the http response
+
+    // We can access the 2D array of the returned API object in this manner:
+    // console.log(data.states);              Returns the entire 2D array in JSON format
+    // console.log(data.states[x])            Returns the array of data for aircraft at index x
+    // console.log(data.states[x][x])         Returns the specific piece of data from aircraft at index x
+    console.log(data.states[0]);
+  }
+
+
+  // Call API function
+  //openCall();
 }
-
-
-// Call API function
-// openCall();
