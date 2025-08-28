d4f947525d868ae8839c23718bf2b0914fee9e69
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-08-28 09:35:29 -0500

    Plane icons, rotation based on heading, visual map additions

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 The plan for this application as of 27AUG25 is to create a web application that utilizes the OpenSky
 API (with account authorization) to display flight details in the greater CSTAT area.
 
+
 ## OpenSky REST API Notes:
 Based on the [REST API Documentation](https://openskynetwork.github.io/opensky-api/rest.html#all-state-vectors), 
 this API call returns an object which is actually a 2D array. The collection of arrays contains arrays that have 
@@ -18,6 +19,14 @@ https://opensky-network.org/api/states/all?lamin=30.0707&lomin=-97.2864&lamax=31
 ... (more JSON) ...
 ["a7f70e","N612KA  ","United States",1756305306,1756305306,-96.5878,30.8221,739.14,false,66.59,339.18,0.33,null,792.48,null,false,0]]}
 ```
+
+
+## Map:
+This [tutorial](https://www.youtube.com/watch?v=NyjMmNCtKf4) is useful for the basics of using Leaflet
+and OpenStreet on your webpage.
+- This [GitHub Repo](https://github.com/bbecquet/Leaflet.RotatedMarker/tree/master) is what allows
+us to rotate the markers by the heading. It's initialized in `map.html` and used in `index.js`
+
 
 ## General Notes:
 - **Latitude:** is the horizontal imaginary lines on the globe, the equator is 0, below is neg. and above is pos.
--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 let USER;
 let PASS;
 
-// CONSTs from HTML Items
-const FLIGHTS = document.getElementById("flights");
+// CONSTs from HTML Items, latyer used to tell which page we're on
+const FLIGHT_PAGE = document.getElementById("flights");
+const MAP_PAGE = document.getElementById("map");
 
 // CONSTs for API
 const LAMIN = 30.0707;
@@ -19,10 +20,33 @@ const FULLURL = BASE + ALLSTATES + `?lamin=${LAMIN}&lomin=${LOMIN}&lamax=${LAMAX
 
 // CONSTs for Map
 let map;
+let boundBox;
 let markers = [];
+const BCDC_LA = 30.6409;
+const BCDC_LO = -96.4876;
 
 
-// This is our DATA GATHERING function
+// Initialize Icons
+var planeIcon = L.icon({
+	iconUrl: 'assets/plane.svg',
+	iconSize: [22, 22],
+	iconAnchor: [11, 11]
+})
+
+var boundPointIcon = L.icon({
+	iconUrl: 'assets/boundaryIcon.svg',
+	iconSize: [22, 22],
+	iconAnchor: [11, 21]
+})
+
+var BCDCIcon = L.icon({
+	iconUrl: 'assets/BCDCIcon.svg',
+	iconSize: [22, 22],
+	iconAnchor: [11, 21]
+})
+
+
+// DATA: This is the API call to OpenSky that gathers our data object
 async function openCall() {
 	try {
 		// Fetch credentials
@@ -48,9 +72,17 @@ async function openCall() {
 	}
 }
 
-// Function for our individual flights
+
+
+// FLIGHT: If it sees we're on index.html, run these functions
+if (FLIGHT_PAGE) {
+	flightData();
+}
+
+
+// Takes the big data gathered from the function at top of file and breaks it down by flight and 
+// creates a separate div on the page for each
 async function flightData() {
-	// Gather the data by calling our data function
 	const DATA = await openCall();
 
 	// If no data is returned, end function
@@ -59,8 +91,9 @@ async function flightData() {
 		return;
 	}
 
-	// Clear out old data if there was any
-	FLIGHTS.innerHTML = "";
+	// Access the flights div on index.html, clear out old data so we can refresh
+	let flights = document.getElementById("flights");
+	flights.innerHTML = "";
 
 	// Each individual flight is pulled from DATA since it's a 2D array, from that we pull specifics
 	for (let i = 0; i < DATA.states.length; i++) {
@@ -72,9 +105,9 @@ async function flightData() {
 		const long = DATA.states[i][5] ? DATA.states[i][5].toFixed(4) : "N/A";
 
 		// With this pulled data about a specific flight, make new boxes for each flight
-		const iFLIGHT = document.createElement("div");
-		iFLIGHT.classList.add("iFlight");					// Add it's CSS, and then below the specific HTML
-		iFLIGHT.innerHTML = `
+		const indivFlight = document.createElement("div");
+		indivFlight.classList.add("indivFlight");					// Add it's CSS, and then below the specific HTML
+		indivFlight.innerHTML = `
 			<h2><u>${callsign}</u></h2>
 			<p><b>Speed:</b>       ${speed}</p>
 			<p><b>Altitude:</b>    ${altitude}</p>
@@ -83,37 +116,37 @@ async function flightData() {
 		`
 
 		// Put the specific flight data "box" into the larger one that displays it nicely on screen
-		FLIGHTS.appendChild(iFLIGHT);
+		flights.appendChild(indivFlight);
 	}
 }
 
-// Call the function to get data and the boxes for index.html
-if (document.getElementById("flights")) {
-	// Only run flight boxes if we are on index.html
-	flightData();
-}
 
 
 
-// MAP
-// This is for map.html, if it sees we're on the map.html page we'll do this code:
-if (document.getElementById("map")) {
-    async function updateMap() {
-		// Get our fresh data
-        const DATA = await openCall();
-
-		// If there's no data then quit, somethings wrong
-        if (!DATA || !DATA.states) return;
-
-        makeMap(DATA);
-    }
-
-    // Initial load
+// MAP: If it sees we're on map.html, run these functions
+if (MAP_PAGE) {
+	// Runs it a single time
     updateMap();
 
-    // Repeat every 5 seconds
-    setInterval(updateMap, 5000);
+    // Repeats for a time amount
+    // setInterval(updateMap, 5000);
 }
+
+
+async function updateMap() {
+	// Get our fresh data from the function
+	const DATA = await openCall();
+
+	// If there's no data then quit, otherwise make the map
+	if (!DATA || !DATA.states) {
+		console.warn("No flight data received");
+		return;
+	} else {
+		console.log("Updated map");
+		makeMap(DATA);
+	}
+}
+
 
 function makeMap(DATA) {
 	// If we don't have a map, make one, if one alread yexists then go on to markers
@@ -126,6 +159,16 @@ function makeMap(DATA) {
 			maxZoom: 19,
 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 		}).addTo(map)
+
+		// Points for the boundaries and location of BCDC
+		L.marker([LAMIN, LOMIN], {icon: boundPointIcon}).addTo(map);
+		L.marker([LAMAX, LOMAX], {icon: boundPointIcon}).addTo(map);
+		boundBox = [[LAMIN, LOMIN], [LAMAX, LOMAX]];
+		L.rectangle(boundBox, {color: "#277CE0", weight: 1}).addTo(map);
+
+		// BCDC Location and radius of m
+		L.marker([BCDC_LA, BCDC_LO], {icon: BCDCIcon}).addTo(map);
+		L.circle([BCDC_LA, BCDC_LO], {radius: 20000, color: "#500000"}).addTo(map);
 	}
 
 	// Clear old markers if there are some
@@ -134,12 +177,20 @@ function makeMap(DATA) {
 
 	// Add markers of where all the planes are
 	for (let i = 0; i < DATA.states.length; i++) {
+		// Gather lat, long, and heading (svg rotation)
 		const lat = DATA.states[i][6].toFixed(4);
 		const long = DATA.states[i][5].toFixed(4);
+		const heading = DATA.states[i][10];
 
-		// If the values are indeed there, then add them to the map and the markers list
-		if (lat && long) {
-			const marker = L.marker([lat, long]).addTo(map)
+		// If the values are indeed there, then add them to the map and the markers list with our ICON
+		// This is where that GitHub package is used to rotate the plane SVG icon
+		if (lat && long && heading) {
+			const marker = L.marker([lat, long], {
+				icon: planeIcon,
+				rotationAngle: heading,
+				rotationOrigin: 'center center'
+			}).addTo(map);
+
 			markers.push(marker);
 		}
 	}
